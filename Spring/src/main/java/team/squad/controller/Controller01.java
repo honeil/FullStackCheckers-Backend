2c879af85fdad34e0d00135abd20a2f75c0bb6a3
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-03-08 12:02:07 -0500

    controller01 pcMove works

--- Spring/src/main/java/team/squad/controller/Controller01.java
+++ Spring/src/main/java/team/squad/controller/Controller01.java
@@ -1,8 +1,7 @@
 package team.squad.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import team.squad.Model.Move;
 import team.squad.Model.MoveHandler;
 
 /**
@@ -12,12 +11,27 @@ import team.squad.Model.MoveHandler;
 @RestController
 public class Controller01
 {
-    @RequestMapping("/generateInitialBoardState")
+    @RequestMapping("/start")
     public java.util.Map generateInitialBoardState()
     {
         return new MoveHandler().generateInitialBoardState();
     }
 
-    
+    @RequestMapping("/pcMove")
+    public java.util.Map generateNewBoardStateFromComputerMove(){
+        return new MoveHandler().generateNewBoardStateFromComputerMove();
+        //return new MoveHandler().generateInitialBoardState();
+
+    }
+
+    //@RequestMapping("/playerMove")//needs POST to pass player move as args
+    // keep @RequestMapping(value = "/playerMove", method = RequestMethod.POST, consumes = {"application/json"})
+    // keep public  java.util.Map generateNewBoardStateFromPlayerMove(@ResponseBody Move move){
+
+        // keep return new MoveHandler().generateNewBoardStateFromPlayerMove();
+        //return new MoveHandler().generateInitialBoardState();
+
+    }
+
 }
 
